f6ffd403ef7347b20241c918b5251665f970eca2
Filip Ostrowski <filip.ostrowski96@gmail.com>
2022-11-08 00:41:41 +0100

    3
    button dziala i zczytuje nick z tego co wpisalem

--- app/src/main/java/com/example/quizzapp/MainActivity.kt
+++ app/src/main/java/com/example/quizzapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.quizzapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var nickName: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nickName = findViewById(R.id.enterNickText)
+    }
+    fun submitNick (view: View?){
+        println(nickName.text)
+
     }
 }
