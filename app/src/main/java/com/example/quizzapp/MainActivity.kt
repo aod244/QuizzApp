be66b7c06093fe4a11bf2570bf0ceda993fb60dc
Filip Ostrowski <filip.ostrowski96@gmail.com>
2022-11-08 23:27:40 +0100

    5

--- app/src/main/java/com/example/quizzapp/MainActivity.kt
+++ app/src/main/java/com/example/quizzapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.quizzapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
@@ -16,9 +18,15 @@ class MainActivity : AppCompatActivity() {
 
         nickName = findViewById(R.id.enterNickText)
 
+        val button = findViewById<Button>(R.id.enterNickButton)
+        button.setOnClickListener(){
+            intent = Intent(this, activity_question::class.java)
+            startActivity(intent)
+
+        }
+
     }
     fun submitNick (view: View?){
         println(nickName.text)
-        setContentView(R.layout.activity_question)
     }
 }
--- app/src/main/java/com/example/quizzapp/activity_question.kt
+++ app/src/main/java/com/example/quizzapp/activity_question.kt
@@ -5,17 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 
 class activity_question : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_question)
-        val goBack = findViewById<Button>(R.id.exitButton)
 
-        goBack.setOnClickListener(){
-            Toast.
+
+
+        val button2 = findViewById<Button>(R.id.exitButton)
+        button2.setOnClickListener(){
+            intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+
         }
     }
 
