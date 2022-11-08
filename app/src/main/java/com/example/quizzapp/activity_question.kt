c2c479a674f71369ce4778b5d31f415a3c869ad2
Filip Ostrowski <filip.ostrowski96@gmail.com>
2022-11-08 01:43:24 +0100

    3
    3

--- app/src/main/java/com/example/quizzapp/activity_question.kt
+++ app/src/main/java/com/example/quizzapp/activity_question.kt
@@ -7,5 +7,7 @@ class activity_question : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
+
+
     }
 }
